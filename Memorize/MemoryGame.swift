a3e1128775fe213e46e5cdd55363a4d5059f30fd
Stefan <stefanclaussen@me.com>
2020-09-03 11:06:28 +0100

    Code ending lecture 2

---
+++ Memorize/MemoryGame.swift
@@ -0,0 +1,31 @@
+
+
+import Foundation
+
+
+struct MemoryGame<CardContent> {
+    
+    var cards: Array<Card>
+    
+    init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
+//        cards = Array<Card>()
+        cards = []
+        for pairIndex in 0..<numberOfPairsOfCards {
+            let content = cardContentFactory(pairIndex)
+            cards.append(Card(content: content, id: pairIndex*2))
+            cards.append(Card(content: content, id: pairIndex*2+1))
+        }
+    }
+    
+    func choose(card: Card) {
+        print("card chosen: \(card)")
+    }
+    
+    struct Card: Identifiable {
+        var isFaceUp: Bool = true
+        var isMatched: Bool = false
+        var content: CardContent
+        var id: Int
+    }
+    
+}
